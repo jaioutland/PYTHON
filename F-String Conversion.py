103d336f2ede7d487b588268e73ca55037465925
Jai Outland <167665241+jaioutland@users.noreply.github.com>
2025-02-04 12:42:07 -0600

    Add files via upload

---
+++ F-String Conversion.py
@@ -0,0 +1,26 @@
+#FString Varable Conversion
+
+def inches_to_cm(inches):
+    return inches * 2.54 
+print(inches_to_cm(31.5))
+print(f"31.5 inches is equal to {inches_to_cm(31.5)} cm.")
+print()
+
+def feet_to_meters(feet):
+    return feet * 0.3048 
+print(feet_to_meters(6))
+print(f"6 meters is equal to {feet_to_meters(6)} meters.")
+print()
+
+def lbs_to_kg(lbs):
+    return lbs * 2.20462
+print(lbs_to_kg (170))
+print(f"170 lbs is equal to {lbs_to_kg(170)} kg.")
+print()
+
+def hours_to_minutes(hrs):
+    return hrs * 60
+print(hours_to_minutes(18))
+print(f"18 hours is equal to {hours_to_minutes(18)} minutes.")
+print()
+
