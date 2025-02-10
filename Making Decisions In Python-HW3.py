8b074f3918e2132c6b09cf526e61c44070f352ba
Jai Outland <167665241+jaioutland@users.noreply.github.com>
2025-02-09 23:00:14 -0600

    Update Making Decisions In Python-HW3.py

--- Making Decisions In Python-HW3.py
+++ Making Decisions In Python-HW3.py
@@ -12,7 +12,20 @@ def can_buy_alcohol(age):
 
 def senior_discount(age):
     return age >= 65
-
+print()
+print(positive_or_negative(10))
+print(positive_or_negative(-3))
+print(is_able_to_vote(8))
+print(is_able_to_vote(28))
+print(can_buy_alcohol(23))
+print(can_buy_alcohol(16))
+print(senior_discount(72))
+print(senior_discount(54))
+print(is_able_to_drive(15))
+print(is_able_to_drive(18))
+print(is_able_to_drive(13))
+print(is_able_to_drive(25))
+print()
 if __name__ == "__main__":
     import doctest
     print(doctest.testfile('doctest3.py'))
