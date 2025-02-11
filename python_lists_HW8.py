b4fb6b9fe6c0908b75a08fda0d4063930a5740da
Jai Outland <167665241+jaioutland@users.noreply.github.com>
2025-02-11 17:11:34 -0600

    Update python_lists_HW8.py

--- python_lists_HW8.py
+++ python_lists_HW8.py
@@ -1,19 +1,13 @@
-'''
-Homework8
-Name: Jai Outland
-github link:
-'''
-
 def sum_numbers(lst):
     # Calculate the sum of all numbers
-    total = 0 
+    total = 0         # Starting with 0
     for num in lst:
-        total += num
+        total += num  # Add each number to the total
     return total     
 
 def largest_number(lst):
     # Find the largest number in a list
-    max_num = lst[0]
+    max_num = lst[0]   # Consider first number to be largest number
     for num in lst:
         if num > max_num: 
             max_num = num
@@ -21,4 +15,5 @@ def largest_number(lst):
 
 if __name__ == "__main__":
     import doctest
-    print(doctest.testfile('doctest8.py'))
+    print(doctest.testfile('doctest8.py'))  # Run the test script and print results
+
