84e511ac82e19e733a26a796884a7c8c594725c5
Jai Outland <167665241+jaioutland@users.noreply.github.com>
2025-02-24 19:25:28 -0600

    Add files via upload

---
+++ Madlib.py
@@ -0,0 +1,31 @@
+Homework Madlib
+Name: Jai Outland
+github link:
+
+def song(verb, feeling, action, object, time, person, emotion, sound):
+    print("Higher")
+    print("\n\n")    
+
+    print(f"I am {verb} higher, and feeling {feeling}")
+    print(f"I'm {action}, on the {object} as the time goes by")
+    print(f"{time},{person} is on my mind")
+    print(f"Feeling {emotion}, I hear the {sound}")
+    print(f"Make me go higher and higher, without drama ")
+
+print('')
+
+# User must input responses
+verb = input("Enter an verb: ")
+feeling = input("Enter a feeling: ")
+action = input("Enter a action: ")
+object = input("Enter a object: ")
+time = input("Enter a time: ")
+person = input("Enter a person: ")
+emotion = input("Enter a emotion: ")
+sound = input("Enter a sound: ")
+
+print('')
+
+song(verb=verb, feeling=feeling, action=action, object=object, time=time, person=person, emotion=emotion, sound=sound)
+
+
