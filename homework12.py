3ac5d781f80b0857025969cbf5c94a4dca02be4f
Jai Outland <167665241+jaioutland@users.noreply.github.com>
2025-02-24 19:28:36 -0600

    Add files via upload

---
+++ homework12.py
@@ -0,0 +1,24 @@
+'''
+Homework12
+Name: Jai Outland
+github link:
+'''
+
+import math
+
+def rectangle (side1, side2):
+# Calculate the area of a rectangle
+    area = side1 * side2
+    return(f'The area of the rectangle is {area} square units')
+
+print('')
+
+def circle (radius):
+# Calculate the area of a circle
+    area = round(math.pi * radius ** 2,1)
+    return(f'The area of a circle is {area:.2f} square units')
+ 
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest12.py'))
