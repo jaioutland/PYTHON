7fee5538916d226cf51e07103a362d19417eaf93
Jai Outland <167665241+jaioutland@users.noreply.github.com>
2025-03-17 21:11:26 -0500

    Add files via upload

---
+++ homework20.py
@@ -0,0 +1,24 @@
+'''
+Homework20
+Name: Jai Outland
+github link:
+'''
+
+# Converts strings to ASCII Value
+
+
+def convert_to_ascii(string):
+    if len(string) == 1:
+        raise ValueError("Enter a single character.")
+    return ord(char)
+
+
+# Filters non-ASCII characters from a string
+
+def filter_non_ascii(string):
+    return "".join(char for char in string if ord(char) < 128).strip()
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest20.py'))
