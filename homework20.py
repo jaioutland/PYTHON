de69bee31465d3588ba810d33ce639f67fcb7152
Jai Outland <167665241+jaioutland@users.noreply.github.com>
2025-03-17 21:21:35 -0500

    Update homework20.py

--- homework20.py
+++ homework20.py
@@ -1,15 +1,19 @@
 # Converts strings to ASCII Value
 
 def convert_to_ascii(string):
-    if len(string) == 1:
+ if len(string) == 1:
+        return ord(string)
+    elif len(string) > 1:
         raise ValueError("Enter a single character.")
-    return ord(char)
-
 
 # Filters non-ASCII characters from a string
 
 def filter_non_ascii(string):
-    return "".join(char for char in string if ord(char) < 128).strip()
+    filter_chars = []
+    for char in string:
+        if ord(char) < 128:
+            filter_chars.append(char)
+    return str("".join(filter_chars)).strip()
 
 
 if __name__ == "__main__":
