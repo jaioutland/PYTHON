5a9cb7a0dbe9b7cde57320fe06963e18d48039e4
Jai Outland <167665241+jaioutland@users.noreply.github.com>
2025-03-22 21:44:48 -0500

    Add files via upload

--- homework25.py
+++ homework25.py
@@ -1,25 +1,25 @@
 '''
-PYTHON Comments - Homework2
+Homework24
 Name: Jai Outland
-github link: https://github.com/jaioutland/PYTHON/blob/main/Python%20Comments-HW25.py
+github link: 
 '''
 
-def birthday(month,day,year):
-    return f" Your birthday is: {month}/ {day}/ {year}"
-print(birthday("September", 12, 1988))
-print()
 
+def flowers(idx, list_of_flowers):
+    try:
+        if not isinstance(idx, int):
+            raise TypeError("Invalid input! Please enter a number.")
 
-def address(street,city,state,zipcode):
-    return  f" Your address is: {street}, {city}, {state}, {zipcode}"
-print(address("435 W 35 St.", "New York", "NY", 11462))
-print()
+        if idx < -len(list_of_flowers) or idx >= len(list_of_flowers):
+            raise IndexError(
+                "Number out of range! Please choose a valid flower number.")
+
+        print(f"You selected: {list_of_flowers[idx]}")
+
+    except (TypeError, IndexError)as e:
+        print(str(e))
 
 
 if __name__ == "__main__":
-    print(birthday("\nMay", 10, 1956))
-    print(birthday("\nDeember", 21, 2002))
-    print(birthday("\nJune", 8, 2013))
-    print(address("\n123 Main St", "Starfall", "AZ", 60547))
-    print(address("\n45 On Lookers Lane", "Ashland", "IL", 98014))
-    print(address("\n845w Mistwwd Ct", "Springfield", "MI", 45017))
+    import doctest
+    print(doctest.testfile('doctest25.py'))
