23b3f525a46eaebdfe8ed5f8c1ee4bc0af6e766c
Jai Outland <167665241+jaioutland@users.noreply.github.com>
2025-04-21 21:39:52 -0500

    Add files via upload

---
+++ word_combinations.py
@@ -0,0 +1,27 @@
+"""
+Jai Outland
+Github:
+"""
+
+# Function that yields all possible two-letter combinations
+
+
+def two_letter_combinations(characters):
+    for first in characters:
+        for second in characters:
+            yield first + second  # Yield will pause function to return the value
+
+# Define 5 letter list
+
+
+def main():
+    chars = ['b', 'e', 'k', 'j', 'm']
+
+# Call and print two-letter combination function
+    print("All possible two-letter combinations:")
+    for combo in two_letter_combinations(chars):
+        print(combo)
+
+
+# Call main function
+main()
